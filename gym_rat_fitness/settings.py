7f577c4362d2520ed7a418508602c87489fea81d
KSheeds1 <katiesheedy@hotmail.com>
2021-12-24 13:29:37 +0000

    Fix error in ALLOWED HOSTS.

--- gym_rat_fitness/settings.py
+++ gym_rat_fitness/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['gym-rat-fitness.herokuapp.com' 'localhost']
+ALLOWED_HOSTS = ['gym-rat-fitness.herokuapp.com', 'localhost']
 
 
 # Application definition
