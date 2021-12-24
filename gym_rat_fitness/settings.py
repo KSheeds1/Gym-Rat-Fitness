024b4eff31dd9e054ade182c055e679c6fbd44c8
KSheeds1 <katiesheedy@hotmail.com>
2021-12-24 13:17:04 +0000

    Correct spelling error in settings file.

--- gym_rat_fitness/settings.py
+++ gym_rat_fitness/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.enivron
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['gym-rat-fitness.herokuapp.com' 'localhost']
 
