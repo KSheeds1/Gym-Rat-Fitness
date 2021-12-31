74c83dce64512cca7f83d612f56a90eabcd1bb67
KSheeds1 <katiesheedy@hotmail.com>
2021-12-31 22:04:16 +0000

    Correct errors in get_products view.

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404
+)
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
@@ -8,7 +10,7 @@ from . models import Product, Category
 
 def get_products(request):
     """
-    A view to show all products including sorting & search queries
+    A view to get all products including sorting & search queries
     """
     products = Product.objects.all()
     query = None
@@ -25,28 +27,25 @@ def get_products(request):
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
                 sortkey = 'category__name'
-
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
+        
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
-        if request.GET:
-            if 'category' in request.GET:
-                categories = request.GET['category'].split(',')
-                products = products.filter(category__name__in=categories)
-                categories = Category.objects.filter(name__in=categories)
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria")
+                return redirect(reverse('products'))
 
-        if request.GET:
-            if 'q' in request.GET:
-                query = request.GET['q']
-                if not query:
-                    messages.error(request, "You didn't enter search criteria")
-                    return redirect(reverse('products'))
-
-                queries = Q(name__icontains=query) | Q(description__icontains=query)
-                products = products.filter(queries)
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
 
