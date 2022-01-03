90b42bbeff28c00699932c045a0b5c050c485853
KSheeds1 <katiesheedy@hotmail.com>
2022-01-03 21:45:21 +0000

    Add views, URLs, amd template for Product Admin and adjust the sort selector placement for medium viewports.

--- products/forms.py
+++ products/forms.py
@@ -15,5 +15,4 @@ class ProductForm(forms.ModelForm):
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = friendly_names
-        for field_name, field in self.fields.items():
-            field.wiget.attrs['class'] = 'border-black'
+
