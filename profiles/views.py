aeadb8027b3c5ae2b3d5566c22385cd0d266cee8
KSheeds1 <katiesheedy@hotmail.com>
2022-01-05 15:40:31 +0000

    Create basic profile template, form, and views.

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,8 @@ from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 
+from checkout.models import Order
+
 
 def profile(request):
     """ Display user profile """
@@ -29,3 +31,23 @@ def profile(request):
     }
 
     return render(request, template, context)
+
+
+def order_history(request, order_number):
+    """ Return information on past orders to the profile """
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number '
+        '{order_number}. A confirmation email was sent on the '
+        'order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+    }
+
+    return render(request, template, context)
+
