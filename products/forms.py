df7979740417e6de21cd9f928dc1afe1143d9763
KSheeds1 <katiesheedy@hotmail.com>
2022-01-19 12:27:21 +0000

    Alter product images and adjust ProductForm.

--- products/forms.py
+++ products/forms.py
@@ -8,7 +8,7 @@ class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        fields = '__all__'
+        exclude = ('rating',)
 
     image = forms.ImageField(label='Image',
                              required=False,
@@ -20,4 +20,28 @@ class ProductForm(forms.ModelForm):
         friendly_names = [(c.id, c.get_friendly_name())
                           for c in categories]
 
+        placeholders = {
+            'category': 'Categories',
+            'sku': 'SKU',
+            'name': 'Name',
+            'description': 'Description',
+            'price': 'Price',
+            'image': 'Image',
+            'image_url': 'Image URL',
+            'length_of_time': 'Length of time',
+            'training_styles': 'Training Styles',
+            'workout_duration': 'Workout Duration',
+            'program_goals': 'Program Goals',
+            'nutrition_plan_features': 'Nutrition Plan Features',
+        }
+
         self.fields['category'].choices = friendly_names
+        for field in self.fields:
+            if field != 'image':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
+            self.fields['image'].label = True
