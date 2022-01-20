6402e99e91add53ab69ef0ff68d0f682e0ba02da
KSheeds1 <katiesheedy@hotmail.com>
2022-01-20 18:01:38 +0000

    Remove unnecesary sorting and mobile filtering.

--- products/views.py
+++ products/views.py
@@ -41,11 +41,6 @@ def get_products(request):
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
 
-        if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
-            products = products.filter(category__name__in=categories)
-            categories = Category.objects.filter(name__in=categories)
-
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
