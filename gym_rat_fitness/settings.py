c663e746ac0b81cd45386fde66728e23f810dae5
KSheeds1 <katiesheedy@hotmail.com>
2022-01-21 00:31:10 +0000

    Reset AWS cache control.

--- gym_rat_fitness/settings.py
+++ gym_rat_fitness/settings.py
@@ -179,10 +179,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Cache Control
-    # AWS_S3_OBJECT_PARAMETERS = {
-    #     'Expires': 'Thu, 17 Dec 2099 20:00:00 GMT',
-    #     'CacheControl': 'max-age=94608000',
-    # }
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 17 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
 
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'gym-rat-fitness'
